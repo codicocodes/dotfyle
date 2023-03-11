2c3617cb6f13b36c1b4a5e0fd78fd83b13dd042c
codico <codicocodes@gmail.com>
2023-03-11 22:05:23 +0100

    Extract GithubLoginButton

---
+++ src/lib/components/GithubLoginButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+</script>
+
+<form>
+	<button
+		class="bg-white/30 text-sm font-semibold p-4 py-2 xl:px-6 xl:py-2 rounded-full flex gap-1 items-center"
+	>
+		<Fa icon={faGithub} /> Get Started <Fa class="ml-1" size="xs" icon={faArrowUpRightFromSquare} />
+	</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-	import Fa from 'svelte-fa';
-	import { faArrowUpRightFromSquare, faBox, faCircle, faPuzzlePiece, faStar } from '@fortawesome/free-solid-svg-icons';
-	import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-	import NeovimConfigCard from '$lib/components/NeovimConfigCard.svelte';
+  import NeovimConfigCard from '$lib/components/NeovimConfigCard.svelte';
+	import GithubLoginButton from '$lib/components/GithubLoginButton.svelte';
 
 	const codiConfig = {
 		stars: 69,
@@ -51,11 +49,7 @@
 			</div>
 
 			<div class="w-full flex justify-center items-center">
-				<button
-					class="bg-white/30 text-sm font-semibold p-4 py-2 xl:px-6 xl:py-2 rounded-full flex gap-1 items-center"
-				>
-					<Fa icon={faGithub} /> Get Started <Fa class="ml-1" size="xs" icon={faArrowUpRightFromSquare} />
-				</button>
+        <GithubLoginButton />
 			</div>
 		</div>
 	</div>
