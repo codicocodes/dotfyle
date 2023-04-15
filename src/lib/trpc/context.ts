25cf46c3997716dd6bda82323f58427fda2189b4
codico <codicocodes@gmail.com>
2023-04-15 19:25:42 +0200

    Remove comment

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -4,9 +4,6 @@ import type { User } from '@prisma/client';
 import type { RequestEvent } from '@sveltejs/kit';
 import type { inferAsyncReturnType } from '@trpc/server';
 
-// we're not using the event parameter is this example,
-// hence the eslint-disable rule
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function createContext(event: RequestEvent) {
   const user = verifyToken(event.cookies)
 	return {
