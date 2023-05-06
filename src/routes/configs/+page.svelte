0004300927805d1323e654eb7716c39025f7c7dc
codico <codicocodes@gmail.com>
2023-05-07 01:23:48 +0200

    Use virtual list in config page

--- src/routes/configs/+page.svelte
+++ src/routes/configs/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
+	import VirtualList from '@sveltejs/svelte-virtual-list';
 	import { page } from '$app/stores';
 	import { trpc } from '$lib/trpc/client';
 	import { onMount } from 'svelte';
-	import CoolText from '$lib/components/CoolText.svelte';
-	import { fade } from 'svelte/transition';
-	import BigGridContainer from '$lib/components/BigGridContainer.svelte';
 	import NeovimConfigCard from '$lib/components/NeovimConfigCard.svelte';
 	import type { NeovimConfigWithMetaData } from '$lib/server/prisma/neovimconfigs/schema';
 	import SmallTitle from '$lib/components/SmallTitle.svelte';
@@ -16,7 +14,7 @@
 	let search = $page.url.searchParams.get('q') ?? '';
 
 	onMount(async () => {
-		const fetchedConfigs = await trpc($page).getConfigs.query();
+		let fetchedConfigs = await trpc($page).getConfigs.query();
 		loading = false;
 		configs = fetchedConfigs as unknown as NeovimConfigWithMetaData[];
 	});
@@ -37,30 +35,35 @@
 
 <div class="w-full flex flex-col items-center px-8">
 	<div class="flex flex-col max-w-5xl w-full gap-4">
-    <SmallTitle title="Find Neovim configs" />
-		<div class="flex items-center justify-center mb-8">
-    <input
+		<SmallTitle title="Find Neovim configs" />
+		<div class="flex items-center justify-center mb-4">
+			<input
 				bind:value={search}
 				class="w-full sm:w-1/2 p-1 sm:p-4 rounded-lg text-black text-lg font-semibold focus:outline-none focus:border-green-500 shadow-xl focus:shadow-green-300/25 focus:ring-1 focus:ring-green-500 bg-white/80"
 			/>
 		</div>
-		<BigGridContainer>
-			{#each filteredConfig as conf}
-				<div in:fade>
+		<div class="flex flex-col h-[calc(100vh-340px)] sm:h-[calc(100vh-320px)]">
+			<!-- 
+          we need to use a virtual list otherwise rerendering is too heavy
+          only way i got it to work was with 100vh - 420px to ensure we don't have double scroll y bars
+          if improving this ensure that there is not double scrollbars on either mobile or desktop
+      -->
+			<VirtualList items={filteredConfig} let:item>
+				<div class="my-2">
 					<NeovimConfigCard
-						slug={conf.slug}
-						repo={conf.repo}
-						owner={conf.owner}
-						avatar={conf.ownerAvatar}
-						initFile={conf.initFile}
-						root={conf.root}
-						stars={conf.stars.toString()}
-						pluginManager={conf.pluginManager ?? 'unknown'}
-						pluginCount={conf.pluginCount.toString()}
-            showGithubLink={false}
+						slug={item.slug}
+						repo={item.repo}
+						owner={item.owner}
+						avatar={item.ownerAvatar}
+						initFile={item.initFile}
+						root={item.root}
+						stars={item.stars.toString()}
+						pluginManager={item.pluginManager ?? 'unknown'}
+						pluginCount={item.pluginCount.toString()}
+						showGithubLink={false}
 					/>
 				</div>
-			{/each}
-		</BigGridContainer>
+			</VirtualList>
+		</div>
 	</div>
 </div>
