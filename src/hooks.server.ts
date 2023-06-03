2d3ea7bb714c0b7a3606fb08d60cacad116417b4
codico <codicocodes@gmail.com>
2023-06-03 19:19:44 +0200

    Handle internal server error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,21 @@
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
 import type { Handle } from '@sveltejs/kit';
+import { TRPCError } from '@trpc/server';
 import { createTRPCHandle } from 'trpc-sveltekit';
+
+export const onError = (opts: any) => {
+	const { error, type, path, input, ctx, req } = opts;
+	if (error.code === 'INTERNAL_SERVER_ERROR') {
+    throw new TRPCError({
+      message: 'Something went wrong',
+      code: 'INTERNAL_SERVER_ERROR'
+    });
+	}
+};
 
 export const handle = createTRPCHandle({
 	router,
 	createContext,
+	onError
 }) satisfies Handle;
