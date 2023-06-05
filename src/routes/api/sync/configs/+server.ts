c2db55fe3735f83c78888c5526af42b88b95b5d6
codico <codicocodes@gmail.com>
2023-06-06 01:28:45 +0200

    Sync repo info in schedule

--- src/routes/api/sync/configs/+server.ts
+++ src/routes/api/sync/configs/+server.ts
@@ -1,6 +1,9 @@
+import { fetchGithubRepositoryByName } from '$lib/server/github/api';
+import { upsertNeovimConfigDTOFactory } from '$lib/server/nvim-sync/services/sync-repo-info';
 import { SyncManagerFactory } from '$lib/server/nvim-sync/services/SyncManager';
-import { getConfigsWithToken } from '$lib/server/prisma/neovimconfigs/service';
+import { getConfigsWithToken, upsertNeovimConfig } from '$lib/server/prisma/neovimconfigs/service';
 import { getAllNeovimPluginNames } from '$lib/server/prisma/neovimplugins/service';
+import type { NeovimConfig } from '@prisma/client';
 import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
 import pLimit from 'p-limit';
 import { AdminRequestValidator } from '../../common/AdminRequestValidator';
@@ -27,8 +30,10 @@ export const GET: RequestHandler = async function (event: RequestEvent) {
 		limit.push(
 			runner(async () => {
 				try {
-					const syncer = await syncFactory.create(_token, config);
-					await syncer.treeSync();
+					await Promise.all([
+						syncRepoInfo(_token, config),
+						syncFactory.create(_token, config).then((syncer) => syncer.treeSync())
+					]);
 					synced++;
 					console.log(`Synced ${synced}/${configs.length} configs`);
 				} catch (e: any) {
@@ -42,3 +47,9 @@ export const GET: RequestHandler = async function (event: RequestEvent) {
 	});
 	return new Response();
 };
+
+async function syncRepoInfo(token: string, config: NeovimConfig) {
+	const repo = await fetchGithubRepositoryByName(token, config.owner, config.repo);
+	const upsertDTO = upsertNeovimConfigDTOFactory(config.owner, config.root, config.initFile, repo);
+	return upsertNeovimConfig(config.userId, upsertDTO);
+}
