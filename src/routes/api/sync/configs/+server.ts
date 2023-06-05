e39bf6dc7dea50a2df83cfed5eb0f2b350bf2b18
codico <codicocodes@gmail.com>
2023-06-06 00:40:20 +0200

    Fix plimit usage

--- src/routes/api/sync/configs/+server.ts
+++ src/routes/api/sync/configs/+server.ts
@@ -1,43 +1,46 @@
-import { SyncManagerFactory, syncManagerFactory } from '$lib/server/nvim-sync/services/SyncManager';
+import { SyncManagerFactory } from '$lib/server/nvim-sync/services/SyncManager';
 import { getConfigsWithToken } from '$lib/server/prisma/neovimconfigs/service';
-import {
-    getAllNeovimPluginNames,
-	searchPlugins,
-} from '$lib/server/prisma/neovimplugins/service';
-import { getAdminGithubToken } from '$lib/server/prisma/users/service';
-import { PluginSyncer } from '$lib/server/sync/plugins/sync';
+import { getAllNeovimPluginNames } from '$lib/server/prisma/neovimplugins/service';
 import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
 import pLimit from 'p-limit';
 import { AdminRequestValidator } from '../../common/AdminRequestValidator';
 
 let running = false;
 
-const runner = pLimit(10)
+const runner = pLimit(10);
 
 export const GET: RequestHandler = async function (event: RequestEvent) {
-	new AdminRequestValidator(event).validate()
-  if (running) {
-    throw error(429, 'limit');
-  }
+	new AdminRequestValidator(event).validate();
+	if (running) {
+		throw error(429, 'limit');
+	}
 	try {
-    running = true
-    const trackedPlugins = await getAllNeovimPluginNames()
-    const syncFactory = new SyncManagerFactory(trackedPlugins)
+		running = true;
+		const trackedPlugins = await getAllNeovimPluginNames();
+		const syncFactory = new SyncManagerFactory(trackedPlugins);
 
 		let synced = 0;
 
-    const configs = await getConfigsWithToken()
+		const configs = await getConfigsWithToken();
+		const limit = [];
 
-		for (const {_token, ...config} of configs) {
-      runner(async () => {
-        const syncer = await syncFactory.create(_token, config)
-        await syncer.treeSync().catch((e) => [console.log(`Failed syncing ${config.slug}`, e)]);
-        synced++;
-        console.log(`Synced ${synced}/${configs.length} configs`);
-      })
+		for (const { _token, ...config } of configs) {
+			limit.push(
+				runner(async () => {
+          try {
+            const syncer = await syncFactory.create(_token, config);
+            await syncer.treeSync()
+            synced++;
+            console.log(`Synced ${synced}/${configs.length} configs`);
+          } catch (e: any) {
+            console.log(`Failed syncing ${config.slug}`, e.message);
+          }
+				})
+			);
 		}
+    await Promise.all(limit)
 		return new Response();
 	} finally {
-    running = false;
-  }
+		running = false;
+	}
 };
--- src/routes/api/sync/plugins/+server.ts
+++ src/routes/api/sync/plugins/+server.ts
@@ -1,6 +1,4 @@
-import {
-	searchPlugins,
-} from '$lib/server/prisma/neovimplugins/service';
+import { searchPlugins } from '$lib/server/prisma/neovimplugins/service';
 import { getAdminGithubToken } from '$lib/server/prisma/users/service';
 import { PluginSyncer } from '$lib/server/sync/plugins/sync';
 import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
@@ -9,30 +7,36 @@ import { AdminRequestValidator } from '../../common/AdminRequestValidator';
 
 let running = false;
 
-const runner = pLimit(10)
+const runner = pLimit(10);
 
 export const GET: RequestHandler = async function (event: RequestEvent) {
-	new AdminRequestValidator(event).validate()
-  if (running) {
-    throw error(429, 'limit');
-  }
+	new AdminRequestValidator(event).validate();
+	if (running) {
+		throw error(429, 'limit');
+	}
 	try {
-    running = true
+		running = true;
 		const [token, plugins] = await Promise.all([getAdminGithubToken(), searchPlugins()]);
 
 		let synced = 0;
 
+		const limit = [];
+
 		for (const plugin of plugins) {
-      runner(async () => {
-        const syncer = new PluginSyncer(token, plugin);
-        await syncer.sync()
-        .catch((e) => [console.log(`Failed syncing ${plugin.owner}/${plugin.name}`, e)]);
-        synced++;
-        console.log(`Synced ${synced}/${plugins.length} plugins`);
-      })
+			limit.push(
+				runner(async () => {
+					const syncer = new PluginSyncer(token, plugin);
+					await syncer
+						.sync()
+						.catch((e) => [console.log(`Failed syncing ${plugin.owner}/${plugin.name}`, e)]);
+					synced++;
+					console.log(`Synced ${synced}/${plugins.length} plugins`);
+				})
+			);
 		}
+    await Promise.all(limit)
 		return new Response();
 	} finally {
-    running = false;
-  }
+		running = false;
+	}
 };
