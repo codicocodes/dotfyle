e481f6bcb154c67bfd147b7047c030b469136ca8
codico <codicocodes@gmail.com>
2023-06-06 01:14:37 +0200

    Prettier

--- src/lib/server/nvim-sync/services/SyncManager.ts
+++ src/lib/server/nvim-sync/services/SyncManager.ts
@@ -12,11 +12,11 @@ import { FileContentTraverser } from './FileContentTraverser';
 import { findLazyLockNode, findPluginManager } from './plugins/syncer';
 
 export class SyncManager {
-  foundPlugins: Set<number> = new Set()
+	foundPlugins: Set<number> = new Set();
 	treeTraverser: FileContentTraverser;
 	syncedPluginManager = false;
 	constructor(
-    token: string,
+		token: string,
 		private tree: GithubTree,
 		public config: NeovimConfig,
 		private trackedPlugins: NeovimPluginIdentifier[]
@@ -32,30 +32,30 @@ export class SyncManager {
 
 	async treeSync(): Promise<NeovimConfigWithPlugins> {
 		const pluginManager = findPluginManager(this.tree, this.config);
-    if (pluginManager) {
-      this.config = await updatePluginManager(this.config.id, pluginManager);
-      this.syncedPluginManager = true;
-      // TODO: simple plugin finder to identify untracked plugins?
-    }
+		if (pluginManager) {
+			this.config = await updatePluginManager(this.config.id, pluginManager);
+			this.syncedPluginManager = true;
+			// TODO: simple plugin finder to identify untracked plugins?
+		}
 		return await this.fileSyncer();
 	}
 
 	async fileSyncer() {
 		for await (const content of this.treeTraverser.traverse()) {
 			await this.syncPluginManager(content);
-      this.findPlugins(content)
-      this.findLeaderKey(content)
+			this.findPlugins(content);
+			this.findLeaderKey(content);
 		}
-      return await addPlugins(this.config.id, [...this.foundPlugins])
+		return await addPlugins(this.config.id, [...this.foundPlugins]);
 	}
 
 	findPlugins(content: string) {
 		for (const plugin of this.trackedPlugins) {
-      const { owner, name } = plugin
-      const fullName = `${owner}/${name}`;
-      if (content.includes(fullName)) {
-        this.foundPlugins.add(plugin.id)
-      }
+			const { owner, name } = plugin;
+			const fullName = `${owner}/${name}`;
+			if (content.includes(fullName)) {
+				this.foundPlugins.add(plugin.id);
+			}
 		}
 	}
 
@@ -100,18 +100,18 @@ export class SyncManager {
 		return false;
 	}
 
-  // TODO: fix this and add parsing for more leaderkeys than space
+	// TODO: fix this and add parsing for more leaderkeys than space
 	findLeaderKey(content: string): string | undefined {
-    for (const line of content.split("\n")) {
-      if (line.includes('mapleader')) {
-        const leaderSplit = line.trim().split("=")
-        if (leaderSplit.length !== 2) continue
-        const leaderKey = leaderSplit[1]
-        // TODO: parse out to fix for trailing '<x>' and "<x>"
-        console.log({ leaderKey })
-        return leaderKey
-      }
-    }
+		for (const line of content.split('\n')) {
+			if (line.includes('mapleader')) {
+				const leaderSplit = line.trim().split('=');
+				if (leaderSplit.length !== 2) continue;
+				const leaderKey = leaderSplit[1];
+				// TODO: parse out to fix for trailing '<x>' and "<x>"
+				console.log({ leaderKey });
+				return leaderKey;
+			}
+		}
 	}
 }
 
@@ -166,14 +166,14 @@ export interface PluginFinder {
 export async function syncManagerFactory(user: User, config: NeovimConfig): Promise<SyncManager> {
 	const token = await getGithubToken(user.id);
 	const tree = await fetchRepoFileTree(token, config.owner, config.repo, config.branch);
-  const trackedPlugins = await getAllNeovimPluginNames()
+	const trackedPlugins = await getAllNeovimPluginNames();
 	return new SyncManager(token, tree, config, trackedPlugins);
 }
 
 export class SyncManagerFactory {
-  constructor(private trackedPlugins: NeovimPluginIdentifier[]) {}
-  async create(token: string, config: NeovimConfig) {
-    const tree = await fetchRepoFileTree(token, config.owner, config.repo, config.branch);
-    return new SyncManager(token, tree, config, this.trackedPlugins);
-  }
+	constructor(private trackedPlugins: NeovimPluginIdentifier[]) {}
+	async create(token: string, config: NeovimConfig) {
+		const tree = await fetchRepoFileTree(token, config.owner, config.repo, config.branch);
+		return new SyncManager(token, tree, config, this.trackedPlugins);
+	}
 }
