33a7f81543ac055ff9e0ff64a5dc11775b2da760
codico <codicocodes@gmail.com>
2023-06-05 23:04:27 +0200

    Exctract AdminRequestValidator

---
+++ src/routes/api/common/AdminRequestValidator.ts
@@ -0,0 +1,40 @@
+import { NODE_ENV } from '$env/static/private';
+import { verifyToken } from '$lib/server/auth/services';
+import { isAdmin } from '$lib/utils';
+import { error, type RequestEvent } from '@sveltejs/kit';
+import { INTERNAL_API_TOKEN } from '$env/static/private';
+import type { User } from '@prisma/client';
+
+export class AdminRequestValidator {
+	event: RequestEvent;
+	constructor(event: RequestEvent) {
+		this.event = event;
+	}
+
+	validate() {
+		if (this.isInternalAPI()) {
+			return;
+		}
+		const user = verifyToken(this.event.cookies);
+    this.validateAdmin(user)
+	}
+
+	isInternalAPI(): boolean {
+		if (!INTERNAL_API_TOKEN) {
+			return false;
+		}
+		return this.event.request.headers.get('Authorization') === INTERNAL_API_TOKEN;
+	}
+
+	validateAdmin(user: User | null) {
+		if (!user) {
+			throw error(401, 'unauthorized');
+		}
+		if (NODE_ENV === 'production') {
+			if (!isAdmin(user)) {
+				throw error(403, 'forbidden');
+			}
+		}
+	}
+}
+
--- src/routes/api/seeder/plugins/+server.ts
+++ src/routes/api/seeder/plugins/+server.ts
@@ -1,47 +1,10 @@
-import { NODE_ENV } from '$env/static/private';
-import { verifyToken } from '$lib/server/auth/services';
 import { upsertManyNeovimPlugins } from '$lib/server/prisma/neovimplugins/service';
 import { scrapeRockerBooAwesomeNeovim } from '$lib/server/seeder/plugins';
-import { isAdmin } from '$lib/utils';
-import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
-import { INTERNAL_API_TOKEN } from '$env/static/private';
-import type { User } from '@prisma/client';
-
-class RequestValidator {
-	event: RequestEvent;
-	constructor(event: RequestEvent) {
-		this.event = event;
-	}
-
-	validate() {
-		if (this.isInternalAPI()) {
-			return;
-		}
-		const user = verifyToken(this.event.cookies);
-    this.validateAdmin(user)
-	}
-
-	isInternalAPI(): boolean {
-		if (!INTERNAL_API_TOKEN) {
-			return false;
-		}
-		return this.event.request.headers.get('Authorization') === INTERNAL_API_TOKEN;
-	}
-
-	validateAdmin(user: User | null) {
-		if (!user) {
-			throw error(401, 'unauthorized');
-		}
-		if (NODE_ENV === 'production') {
-			if (!isAdmin(user)) {
-				throw error(403, 'forbidden');
-			}
-		}
-	}
-}
+import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import { AdminRequestValidator } from '../../common/AdminRequestValidator';
 
 export const GET: RequestHandler = async function (event: RequestEvent) {
-	new RequestValidator(event).validate()
+	new AdminRequestValidator(event).validate()
 	const plugins = await scrapeRockerBooAwesomeNeovim();
 	const saved = await upsertManyNeovimPlugins(plugins);
 	const res = new Response(JSON.stringify(saved, undefined, 4));
