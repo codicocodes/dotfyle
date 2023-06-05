b5dad1c8cc770d1b25f904259ce6d6ac12e41b26
codico <codicocodes@gmail.com>
2023-06-05 22:45:28 +0200

    Validate internal api token

--- src/routes/api/seeder/plugins/+server.ts
+++ src/routes/api/seeder/plugins/+server.ts
@@ -1,28 +1,49 @@
-import { NODE_ENV } from "$env/static/private";
-import { verifyToken } from "$lib/server/auth/services";
-import { upsertManyNeovimPlugins } from "$lib/server/prisma/neovimplugins/service";
-import { scrapeRockerBooAwesomeNeovim } from "$lib/server/seeder/plugins";
-import { isAdmin } from "$lib/utils";
-import { error, type RequestEvent, type RequestHandler } from "@sveltejs/kit";
+import { NODE_ENV } from '$env/static/private';
+import { verifyToken } from '$lib/server/auth/services';
+import { upsertManyNeovimPlugins } from '$lib/server/prisma/neovimplugins/service';
+import { scrapeRockerBooAwesomeNeovim } from '$lib/server/seeder/plugins';
+import { isAdmin } from '$lib/utils';
+import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
+import { INTERNAL_API_TOKEN } from '$env/static/private';
+import type { User } from '@prisma/client';
 
-function validateAdmin(event: RequestEvent) {
-  const user = verifyToken(event.cookies)
-  if (!user) {
-    throw error(401, 'unauthorized')
-  }
-  if (NODE_ENV === 'production') {
-    if (!isAdmin(user)) {
-      throw error(403, 'forbidden')
-    }
-  }
+class RequestValidator {
+	event: RequestEvent;
+	constructor(event: RequestEvent) {
+		this.event = event;
+	}
+
+	validate() {
+		if (this.isInternalAPI()) {
+			return;
+		}
+		const user = verifyToken(this.event.cookies);
+    this.validateAdmin(user)
+	}
+
+	isInternalAPI(): boolean {
+		if (!INTERNAL_API_TOKEN) {
+			return false;
+		}
+		return this.event.request.headers.get('Authorization') === INTERNAL_API_TOKEN;
+	}
+
+	validateAdmin(user: User | null) {
+		if (!user) {
+			throw error(401, 'unauthorized');
+		}
+		if (NODE_ENV === 'production') {
+			if (!isAdmin(user)) {
+				throw error(403, 'forbidden');
+			}
+		}
+	}
 }
 
 export const GET: RequestHandler = async function (event: RequestEvent) {
-  validateAdmin(event)
-  const plugins = await scrapeRockerBooAwesomeNeovim()
-  const saved = await upsertManyNeovimPlugins(plugins)
-  const res = new Response(
-      JSON.stringify(saved, undefined, 4)
-  )
-  return res;
+	new RequestValidator(event).validate()
+	const plugins = await scrapeRockerBooAwesomeNeovim();
+	const saved = await upsertManyNeovimPlugins(plugins);
+	const res = new Response(JSON.stringify(saved, undefined, 4));
+	return res;
 };
