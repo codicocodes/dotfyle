90feffd494c5eee323c2eb2888852fb00eeff34c
codico <codicocodes@gmail.com>
2023-06-08 13:54:46 +0200

    Simplify frontpage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,20 +3,14 @@ import type { PageServerLoad, PageServerLoadEvent } from './$types';
 import type { NeovimConfigWithMetaData } from '$lib/server/prisma/neovimconfigs/schema';
 
 export const load: PageServerLoad = async function load(event: PageServerLoadEvent) {
-	const [configs, newPlugins, popularPlugins, colorschemes, editingSupport, preConfigured] = await Promise.all([
+	const [configs, newPlugins, popularPlugins] = await Promise.all([
 		trpc(event).getNewestConfigs.query() as unknown as NeovimConfigWithMetaData[],
-    trpc(event).searchPlugins.query({ sorting: 'new', take: 3 }),
+    trpc(event).searchPlugins.query({ sorting: 'new', take: 6 }),
     trpc(event).searchPlugins.query({ sorting: 'popular', take: 3 }),
-		trpc(event).searchPlugins.query({ category: 'colorscheme', sorting: "popular", take: 3 }),
-    trpc(event).searchPlugins.query({ category: 'editing-support', sorting: 'popular', take: 3 }),
-    trpc(event).searchPlugins.query({ category: 'preconfigured', sorting: 'popular', take: 3 }),
 	]);
 	return {
 		configs,
     newPlugins,
 		popularPlugins,
-		colorschemes,
-    editingSupport,
-    preConfigured
 	};
 };
