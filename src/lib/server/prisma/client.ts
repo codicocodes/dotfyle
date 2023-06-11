be15239c12479a6def9f5116be44fbf48b6546b9
codico <codicocodes@gmail.com>
2023-06-12 01:57:19 +0200

    Before exit hook logger

--- src/lib/server/prisma/client.ts
+++ src/lib/server/prisma/client.ts
@@ -3,3 +3,8 @@ import Prisma from '@prisma/client';
 const { PrismaClient } = Prisma;
 
 export const prismaClient = new PrismaClient();
+
+prismaClient.$on('beforeExit', async () => {
+  console.log('beforeExit hook')
+})
+
