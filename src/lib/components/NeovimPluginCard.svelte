1bdcb33363c90ca34276812f9624968e51a83e52
codico <codicocodes@gmail.com>
2023-06-23 18:40:49 +0200

    Enable showing metadata on right side of plugin card

--- src/lib/components/NeovimPluginCard.svelte
+++ src/lib/components/NeovimPluginCard.svelte
@@ -1,46 +1,43 @@
 <script lang="ts">
-	import Fa from 'svelte-fa';
-	import { faHashtag, faStar, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 	import CoolTextOnHover from './CoolTextOnHover.svelte';
+	import NeovimPluginMetaData from './NeovimPluginMetaData.svelte';
 	export let owner: string;
 	export let name: string;
 	export let stars: string;
 	export let configCount: number;
 	export let category: string;
 	export let shortDescription: string;
+	export let size: 'sm' | 'lg' = 'sm';
 </script>
 
 <div
-	class="h-full relative flex flex-col justify-between overflow-hidden rounded-md border border-green-300/25 bg-white/5 transition-colors w-full shadow-lg hover:shadow-green-300/25"
+	class="h-full relative flex flex-col justify-between overflow-hidden rounded-md border border-green-300/25 bg-white/10 hover:bg-white/20 transition-colors w-full shadow-lg hover:shadow-green-300/25"
 >
-	<div
-		class="h-full flex space-x-4 bg-white/10 p-2 pl-5 transition-colors hover:bg-white/20 items-start"
-	>
-		<div class="flex flex-col gap-1">
-    <CoolTextOnHover>
-			<a href={`/plugins/${owner}/${name}`} class="text-sm font-semibold tracking-wide md:text-base">
-				{owner}/{name}
-			</a>
-    </CoolTextOnHover>
-    {#if shortDescription}
-			<p class="text-xs font-medium tracking-wide md:text-sm">
-				{shortDescription}
-			</p>
-    {/if}
+	<div class="h-full flex space-x-4 p-2 pl-5 transition-colors items-start">
+		<div class="w-full flex flex-col gap-1">
+			<div class="flex w-fulll justify-between">
+				<CoolTextOnHover>
+					<a
+						href={`/plugins/${owner}/${name}`}
+						class="text-sm font-semibold tracking-wide md:text-base"
+					>
+						{owner}/{name}
+					</a>
+				</CoolTextOnHover>
+				{#if size !== 'sm'}
+					<div class="hidden sm:flex">
+						<NeovimPluginMetaData {stars} {configCount} {category} />
+					</div>
+				{/if}
+			</div>
+			{#if shortDescription}
+				<p class="text-xs font-medium tracking-wide md:text-sm">
+					{shortDescription}
+				</p>
+			{/if}
 		</div>
 	</div>
-	<div class="m-1 flex font-medium">
-		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
-			<Fa icon={faStar} />
-			{stars}
-		</span>
-		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
-			<Fa icon={faUserGroup} />
-			{configCount}
-		</span>
-		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
-			<Fa icon={faHashtag} />
-			{category}
-		</span>
+	<div class={size === "sm" ? "px-2" : "sm:hidden"}> 
+		<NeovimPluginMetaData {stars} {configCount} {category} />
 	</div>
 </div>
---
+++ src/lib/components/NeovimPluginMetaData.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { faHashtag, faStar, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+
+
+	import Fa from "svelte-fa";
+  
+  export let stars: string
+  export let configCount: number
+  export let category: string
+
+</script>
+	<div class="m-1 flex font-medium">
+		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
+			<Fa icon={faStar} />
+			{stars}
+		</span>
+		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
+			<Fa icon={faUserGroup} />
+			{configCount}
+		</span>
+		<span class="px-2 py-1 rounded-full text-xs flex gap-1 items-center font-semibold">
+			<Fa icon={faHashtag} />
+			{category}
+		</span>
+	</div>
