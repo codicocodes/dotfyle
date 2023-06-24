5b9e083105156cc4450aecbf476d22c2c8b23a37
codico <codicocodes@gmail.com>
2023-06-24 17:23:49 +0200

    Fix PostCard.svelte font weight

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -2,7 +2,6 @@
 	import { humanizeAbsolute } from "$lib/utils";
 	import { faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 import Fa from "svelte-fa";
-	import CoolTextWithChildren from "./CoolTextWithChildren.svelte";
 	import CoolTextOnHover from "./CoolTextOnHover.svelte";
 
 	export let title: string;
@@ -12,7 +11,7 @@ import Fa from "svelte-fa";
 </script>
 
 <div
-	class="relative flex flex-col justify-between overflow-hidden rounded-md border border-green-300/25 bg-white/10 transition-colors w-full shadow-lg hover:shadow-green-300/25 transition-colors bg-white/10 hover:bg-white/20 "
+	class="relative flex flex-col justify-between overflow-hidden rounded-md border border-green-300/25 bg-white/10 transition-colors w-full shadow-lg hover:shadow-green-300/25 hover:bg-white/20 "
 >
 	<div class="flex items-center space-x-2 pt-2 py-1 pl-5">
   <CoolTextOnHover>
@@ -25,12 +24,12 @@ import Fa from "svelte-fa";
   </CoolTextOnHover>
 	</div>
 	<div class="flex items-center space-x-2 pl-5">
-  <div class="text-xs font-thin tracking-wide">
+  <div class="text-xs font-normal tracking-wide">
 {humanizeAbsolute(date)}
 </div>
 </div>
 	<div class="flex items-center space-x-2 p-2 pl-5 h-full">
-		<div class="text-sm font-thin tracking-wide">
+		<div class="text-sm font-normal tracking-wide">
 		  {text}
 		</div>
 	</div>
