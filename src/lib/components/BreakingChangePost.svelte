c3e5b998629c9ecc1db873d30a155b001ee7ef65
codico <codicocodes@gmail.com>
2023-06-25 13:48:52 +0200

    Add link to commit for breaking changes

--- src/lib/components/BreakingChangePost.svelte
+++ src/lib/components/BreakingChangePost.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { JsonBreakingChangePost } from '$lib/types';
 	import { humanizeAbsolute } from '$lib/utils';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
 	import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
+	import CoolText from './CoolText.svelte';
 	import CoolTextOnHover from './CoolTextOnHover.svelte';
 
 	export let post: JsonBreakingChangePost;
@@ -11,6 +13,8 @@
 	const url = `/plugins/${post.breakingChange.plugin.owner}/${post.breakingChange.plugin.name}`;
 	const title = `Breaking change in ${post.breakingChange.plugin.name}`;
 	const text = post.title;
+	const githubLink = post.breakingChange.externalUrl;
+	const shortSha = post.breakingChange.sha.substring(0, 6);
 </script>
 
 <div
@@ -27,13 +31,17 @@
 		</CoolTextOnHover>
 	</div>
 	<div class="flex items-center space-x-2 pl-5">
-		<div class="text-xs font-normal tracking-wide">
+		<div class="flex gap-2 text-sm font-normal tracking-wide">
+			<a class="flex gap-1 w-auto items-center" href={githubLink} target="_blank">
+				<Fa size="sm" icon={faGithub} />
+				<CoolText text={shortSha} />
+			</a>
 			{humanizeAbsolute(date)}
 		</div>
 	</div>
 	<div class="flex items-center space-x-2 p-2 pl-5 h-full">
-		<div class="text-sm font-normal tracking-wide">
+		<span class="text-sm font-normal tracking-wide flex gap-2">
 			{text}
-		</div>
+		</span>
 	</div>
 </div>
