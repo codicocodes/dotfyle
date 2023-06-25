567fdfdf501e55c10291d7e779504f574deb26bd
codico <codicocodes@gmail.com>
2023-06-25 14:46:48 +0200

    Speedup plugin search page

--- src/routes/plugins/+page.ts
+++ src/routes/plugins/+page.ts
@@ -5,18 +5,23 @@ export const load: PageLoad = async function load(event: PageLoadEvent) {
 	const query = event.url.searchParams.get('q') ?? undefined;
 	const rawPage = event.url.searchParams.get('page') ?? '';
 	const page = parseInt(rawPage, 10);
-	const sorting = (event.url.searchParams.get('sort') ?? 'popular') as 'new' | 'popular' | 'trending';
+	const sorting = (event.url.searchParams.get('sort') ?? 'popular') as
+		| 'new'
+		| 'popular'
+		| 'trending';
+
 	const selectedCategories =
 		event.url.searchParams.get('categories')?.split(',').filter(Boolean) ?? [];
 
-	const res = await trpc(event).searchPlugins.query({
-		query,
-		sorting,
-		categories: selectedCategories,
-		page: isNaN(page) ? 1 : page
-	});
-
-	const categories = await trpc(event).listPluginCategories.query();
+	const [res, categories] = await Promise.all([
+		trpc(event).searchPlugins.query({
+			query,
+			sorting,
+			categories: selectedCategories,
+			page: isNaN(page) ? 1 : page
+		}),
+		trpc(event).listPluginCategories.query()
+	]);
 
 	return {
 		plugins: res.data,
