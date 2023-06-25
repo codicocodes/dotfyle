401f228474aa567a1d03679c43bd31194472bd96
codico <codicocodes@gmail.com>
2023-06-25 14:45:55 +0200

    Speedup config search

--- src/routes/configs/+page.ts
+++ src/routes/configs/+page.ts
@@ -5,27 +5,27 @@ export const load: PageLoad = async function load(event: PageLoadEvent) {
 	const query = event.url.searchParams.get('q') ?? undefined;
 	const rawPage = event.url.searchParams.get('page') ?? '';
 	const page = parseInt(rawPage, 10);
-	const sorting = (event.url.searchParams.get('sort') ?? 'new') as
-		| 'new'
-		| 'stars'
-		| 'plugins';
+	const sorting = (event.url.searchParams.get('sort') ?? 'new') as 'new' | 'stars' | 'plugins';
 
 	const rawPluginIdentifiers = event.url.searchParams.get('plugins');
-	const plugins =
+	const pluginFilter =
 		rawPluginIdentifiers === '' ? [] : rawPluginIdentifiers?.split(',').filter(Boolean);
 
-	const res = await trpc(event).searchConfigs.query({
-		query,
-    sorting,
-		plugins,
-    page: isNaN(page) ? 1 : page,
-	});
+	const [res, plugins] = await Promise.all([
+		trpc(event).searchConfigs.query({
+			query,
+			sorting,
+			plugins: pluginFilter,
+			page: isNaN(page) ? 1 : page
+		}),
+		trpc(event)
+			.getPluginIdentifiers.query()
+			.then((names) => names.map((identifier) => `${identifier.owner}/${identifier.name}`))
+	]);
 
 	return {
 		configs: res.data,
-    pagination: res.meta,
-		plugins: await trpc(event)
-			.getPluginIdentifiers.query()
-			.then((names) => names.map((identifier) => `${identifier.owner}/${identifier.name}`))
+		pagination: res.meta,
+		plugins
 	};
 };
