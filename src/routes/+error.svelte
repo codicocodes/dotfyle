6e2897728b3d684889fa0d72b2c0f05354011597
codico <codicocodes@gmail.com>
2023-06-30 00:10:13 +0200

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import SmallTitle from '$lib/components/SmallTitle.svelte';
+	console.log($page.error);
+	function is404(message?: string) {
+		return message?.includes('404');
+	}
+</script>
+
+<div class="w-full flex items-center justify-center">
+	{#if is404($page.error?.message)}
+		<SmallTitle title="Not Found" />
+  {:else}
+		<SmallTitle title="Error" />
+	{/if}
+</div>
