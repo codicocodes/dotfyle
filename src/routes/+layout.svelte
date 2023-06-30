2b17d7b98189d88d61aa7e60b30c12f710c139e4
codico <codicocodes@gmail.com>
2023-06-30 17:09:47 +0200

    Add hamburger menu for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	import OuterLayout from '$lib/components/OuterLayout.svelte';
 	import { isAdmin } from '$lib/utils';
 	import { faDiscord, faGithub, faTwitch } from '@fortawesome/free-brands-svg-icons';
-	import { faKeyboard, faSignOut, faSync, faUser } from '@fortawesome/free-solid-svg-icons';
+	import { faBars, faKeyboard, faSignOut, faSync, faUser } from '@fortawesome/free-solid-svg-icons';
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import Fa from 'svelte-fa';
 	import { DoubleBounce } from 'svelte-loading-spinners';
@@ -17,6 +17,7 @@
 	import type { LayoutData } from './$types';
 	import nProgress from 'nprogress';
 	import { onMount } from 'svelte';
+	import Modal from '$lib/components/Modal.svelte';
 	export let data: LayoutData;
 	$: ({ user } = data);
 
@@ -54,13 +55,32 @@
 			nProgress.start();
 		} else nProgress.done();
 	}
-
 	onMount(() => {
 		fetch('/api/auth/refresh', { method: 'POST' }).then((r) => {
 			console.log(r.status);
 		});
 	});
+
+	let showNavModal = false;
 </script>
+
+<Modal showModal={showNavModal} onClose={() => (showNavModal = false)}>
+	<div class="my-4 flex flex-col gap-2">
+		<a
+			href="/configs"
+			class="bg-white/30 text-sm font-semibold p-4 py-2 xl:px-6 xl:py-2 rounded-full flex gap-4 items-center hover:bg-gradient-to-br hover:from-cyan-500 hover:to-green-500 shadow-xl hover:shadow-green-300/25 justify-center"
+		>
+			Configs
+		</a>
+		<a
+			href="/plugins"
+			class="bg-white/30 text-sm font-semibold p-4 py-2 xl:px-6 xl:py-2 rounded-full flex gap-4 items-center hover:bg-gradient-to-br hover:from-cyan-500 hover:to-green-500 shadow-xl hover:shadow-green-300/25 justify-center"
+		>
+			Plugins
+		</a>
+		<GithubLoginButton />
+	</div>
+</Modal>
 
 <svelte:head>
 	<meta property="og:type" content="website" />
@@ -81,21 +101,26 @@
 {/if}
 <div>
 	<div class="flex items-center justify-between w-full px-8 gap-4 mt-2">
-		<div class="flex items-center gap-4 sm:gap-12">
-			<a href="/" class="text-2xl tracking-tight font-black">
-				<CoolText text="dotfyle" />
-			</a>
-			<div class="flex items-center gap-2 sm:gap-4">
-				<a href="/plugins" class="text-lg tracking-tight font-black">
-					<CoolTextOnHover>plugins</CoolTextOnHover>
+		<div class="flex items-center w-full">
+			<div class="flex w-full gap-12 grow">
+				<a href="/" class="text-2xl tracking-tight font-black">
+					<CoolText text="dotfyle" />
 				</a>
+				<div class="hidden sm:flex items-center gap-2 sm:gap-4 text-lg tracking-tight font-semibold">
+					<a href="/plugins">
+						<CoolTextOnHover>plugins</CoolTextOnHover>
+					</a>
 
-				<a href="/configs" class="text-lg tracking-tight font-black">
-					<CoolTextOnHover>configs</CoolTextOnHover>
-				</a>
+					<a href="/configs">
+						<CoolTextOnHover>configs</CoolTextOnHover>
+					</a>
+				</div>
 			</div>
 		</div>
-		<div class="text-sm font-semibold">
+		<div class="flex gap-4 text-sm font-semibold items-center">
+			<button on:click={() => (showNavModal = true)}>
+				<Fa size="xl" class="block: sm:hidden h-full" icon={faBars} />
+			</button>
 			{#if user}
 				<button on:click={toggle}>
 					<div class="flex items-center gap-2 text-lg">
@@ -105,70 +130,62 @@
 						<Avatar src={user.avatarUrl} width="w-10" />
 					</div>
 				</button>
-				{#if isOpen}
-					<div class="absolute right-0 w-40 mt-2 mr-6 z-50">
-						<GlossyCard>
-							<div class="flex flex-col w-full h-full bg-gray-900 sm:bg-transparent">
-								{#if isAdmin(user)}
-									<CoolTextOnHover>
-										<button class="px-4 py-2 flex gap-2 items-center" on:click={syncPlugins}>
-											<div class="force-white-text">
-												{#if syncing}
-													<DoubleBounce color="#15be97" size="8" />
-												{:else}
-													<Fa icon={faSync} />
-												{/if}
-											</div>
-											Sync plugins
-										</button>
-									</CoolTextOnHover>
-								{/if}
-								<CoolTextOnHover>
-									<a
-										on:click={close}
-										href={`/${user.username}`}
-										class="px-4 py-2 flex gap-2 items-center"
-									>
-										<div class="force-white-text">
-											<Fa icon={faUser} />
-										</div>
-										Profile</a
-									>
-								</CoolTextOnHover>
-								<CoolTextOnHover>
-									<button class="px-4 py-2 flex gap-2 items-center" on:click={logout}>
-										<div class="force-white-text">
-											<Fa icon={faSignOut} />
-										</div>
-
-										Logout
-									</button>
-								</CoolTextOnHover>
-							</div>
-						</GlossyCard>
-					</div>
-				{/if}
 			{:else}
-				<div class="hidden sm:block">
+				<div class="hidden sm:flex">
 					<GithubLoginButton />
-				</div>
-				<div
-					class="text-black sm:hidden flex items-center bg-gradient-to-br from-cyan-500 to-green-500 px-2 py-1 rounded-full"
-				>
-					<a href={'/api/auth/github'} class="flex items-center gap-1">
-						<Fa icon={faGithub} />
-						auth
-					</a>
 				</div>
 			{/if}
 		</div>
 	</div>
+
+	{#if isOpen}
+		<div class="absolute right-0 w-40 mt-2 mr-6 z-50">
+			<GlossyCard>
+				<div class="flex flex-col w-full h-full bg-gray-900 sm:bg-transparent">
+					{#if isAdmin(user)}
+						<CoolTextOnHover>
+							<button class="px-4 py-2 flex gap-2 items-center" on:click={syncPlugins}>
+								<div class="force-white-text">
+									{#if syncing}
+										<DoubleBounce color="#15be97" size="8" />
+									{:else}
+										<Fa icon={faSync} />
+									{/if}
+								</div>
+								Sync plugins
+							</button>
+						</CoolTextOnHover>
+					{/if}
+					<CoolTextOnHover>
+						<a
+							on:click={close}
+							href={`/${user.username}`}
+							class="px-4 py-2 flex gap-2 items-center"
+						>
+							<div class="force-white-text">
+								<Fa icon={faUser} />
+							</div>
+							Profile</a
+						>
+					</CoolTextOnHover>
+					<CoolTextOnHover>
+						<button class="px-4 py-2 flex gap-2 items-center" on:click={logout}>
+							<div class="force-white-text">
+								<Fa icon={faSignOut} />
+							</div>
+
+							Logout
+						</button>
+					</CoolTextOnHover>
+				</div>
+			</GlossyCard>
+		</div>
+	{/if}
 	<OuterLayout>
 		<slot />
 	</OuterLayout>
 
 	<footer class="sticky mx-auto max-w-full xl:max-w-7xl w-full sm:mt-4 sm:px-6 lg:px-8">
-		
 		<div class="flex items-center justify-center">
 			<div class="flex gap-4 text-gray-400 items-center">
 				<a
