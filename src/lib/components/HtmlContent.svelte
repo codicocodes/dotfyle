98d016471c76fb4cf523d8afffa0f070bfe93149
codico <codicocodes@gmail.com>
2023-07-17 19:26:19 +0200

    Make headers bigger in html HtmlContent

--- src/lib/components/HtmlContent.svelte
+++ src/lib/components/HtmlContent.svelte
@@ -9,16 +9,16 @@
 
 <style lang="postcss">
 	:global(#markdown-post-container h1) {
-		@apply text-lg sm:text-4xl font-semibold tracking-wider my-4 break-words;
+		@apply text-4xl sm:text-6xl font-semibold tracking-wider my-8 break-words;
 	}
 	:global(#markdown-post-container h2) {
-		@apply sm:text-3xl font-semibold tracking-wider my-2;
+		@apply text-3xl sm:text-5xl font-semibold tracking-wider my-4;
 	}
 	:global(#markdown-post-container h3) {
-		@apply sm:text-2xl font-semibold tracking-wider my-1;
+		@apply text-2xl sm:text-4xl font-semibold tracking-wider my-4;
 	}
 	:global(#markdown-post-container h4) {
-		@apply sm:text-xl font-semibold tracking-wider my-1;
+		@apply text-xl sm:text-3xl font-semibold tracking-wider my-2;
 	}
 	:global(#markdown-post-container p) {
 		@apply text-sm sm:text-lg my-2 gap-2 break-words;
@@ -36,7 +36,7 @@
 		@apply my-8;
 	}
 	:global(#markdown-post-container blockquote) {
-		@apply bg-gray-800 p-4 border-l-4 my-4;
+		@apply bg-gray-800 p-2 border-l-4 my-2;
 	}
 	:global(#markdown-post-container code) {
 		@apply bg-black p-1 text-white;
