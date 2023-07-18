9b442914d5e6a091386439fa681f19071eb266ce
codico <codicocodes@gmail.com>
2023-07-18 19:34:26 +0200

    Propagate style query parameter

--- src/routes/plugins/[owner]/[plugin]/shield/+server.ts
+++ src/routes/plugins/[owner]/[plugin]/shield/+server.ts
@@ -2,15 +2,17 @@ import { getPlugin } from '$lib/server/prisma/neovimplugins/service';
 import type { RequestEvent, RequestHandler } from './$types';
 
 const BASE_URL =
-	'https://img.shields.io/badge/{plugin-usage}-22c55e?logo=neovim&label=configs%20using%20{repo-name}&labelColor=1e40af&link=https://dotfyle.com/plugins/{repo-owner}/{repo-name}&style=plastic';
+	'https://img.shields.io/badge/{plugin-usage}-22c55e?logo=neovim&label=configs%20using%20{repo-name}&labelColor=1e40af&link=https://dotfyle.com/plugins/{repo-owner}/{repo-name}&style={style}';
 
 export const GET: RequestHandler = async function (event: RequestEvent) {
 	const { owner, plugin: plugin } = event.params;
+  const style = event.url.searchParams.get('style') ?? 'flat'
 	const neovimPlugin = await getPlugin(owner, plugin);
 	const usage = neovimPlugin.configCount;
 	const url = BASE_URL.replaceAll('{repo-owner}', owner)
 		.replaceAll('{repo-name}', plugin)
-		.replaceAll('{plugin-usage}', usage.toString());
+		.replaceAll('{plugin-usage}', usage.toString())
+		.replaceAll('{style}', style);
 	const res = await fetch(url).then((r) => r.text());
 	event.setHeaders({
 		'Content-Type': 'image/svg+xml',
