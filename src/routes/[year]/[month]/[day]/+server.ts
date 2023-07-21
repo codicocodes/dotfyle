d9574480e5fbb1ca3225a9283cdaddf720351833
codico <codicocodes@gmail.com>
2023-07-22 00:18:06 +0200

    Remove root level TWiN proxy

--- src/routes/[year]/[month]/[day]/+server.ts
+++
@@ -1,8 +0,0 @@
-
-import { redirect, type RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = (event) => {
-	const { year, month, day } = event.params;
-  console.log({ year, month, day})
-	throw redirect(302, `/twin/${year}/${month}/${day}`);
-};
