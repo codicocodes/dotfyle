0a45b8944c511391bdc2061a9e482dd521f6ee48
codico <codicocodes@gmail.com>
2023-07-26 20:51:26 +0200

    Update readme guide

--- src/routes/guides/auto-generated-readme/+page.svelte
+++ src/routes/guides/auto-generated-readme/+page.svelte
@@ -7,12 +7,13 @@
 	import NeovimConfigCard from '$lib/components/NeovimConfigCard.svelte';
 
 	import type { PageData } from './$types';
+	import GithubLoginButton from '$lib/components/GithubLoginButton.svelte';
 
 	export let data: PageData;
 
-  const url = `https://dotfyle.com/{username}/{slug}/readme`
+	const url = `https://dotfyle.com/{username}/{slug}/readme`;
 
-  const profileUrl = `https://dotfyle.com/{username}`
+	const profileUrl = `https://dotfyle.com/{username}`;
 
 	const readmeStructure = `# Title
 - plugin count badge
@@ -24,6 +25,10 @@
 ## Plugins
 
 ## Language Servers`;
+
+	const badgesHtml = `<a href="https://dotfyle.com/{username}/{slug}"><img src="https://dotfyle.com/{owner}/{slug}/badges/plugins?style={style}" /></a>
+<a href="https://dotfyle.com/{username}/{slug}"><img src="https://dotfyle.com/{username}/{slug}/badges/leaderkey?style={style}" /></a>
+<a href="https://dotfyle.com/{username}/{slug}"><img src="https://dotfyle.com/{username}/{slug}/badges/plugin-manager?style={style}" /></a>`;
 </script>
 
 <svelte:head>
@@ -46,47 +51,57 @@
 			>
 			<Highlight language={markdown} code={readmeStructure} />
 			<span class="inline whitespace-normal">
-        You can change the markdown to your needs before saving it to GitHub.
-      </span>
-		</section>
-	</GlossyCard>
-	<GlossyCard>
-		<section class="flex flex-col gap-2 p-4 w-full">
-			<h3>How to get your README markdown</h3>
-			<span class="inline whitespace-normal">
-				Navigate to your Neovim config on Dotfyle at (<code class="bg-black px-1 py-0.5 rounded">profile > config </code>) and add <code class="bg-black px-1 py-0.5 rounded">/readme</code> to the end of the url.
+				You can change the markdown to your needs before saving it to GitHub.
 			</span>
-			<span class="inline whitespace-normal">
-				Copy the markdown from this page and use it on your GitHub readme.
+
+			<span class="whitespace-normal">
+				If you don't want to use the generated readme, you can use just the Dotfyle badges
+				showcasing the number of plugins installed, your plugin manager, and your leaderkey.
 			</span>
 		</section>
 	</GlossyCard>
 
+	<GlossyCard>
+		<section class="flex flex-col gap-2 p-4 w-full">
+			<h3>How to find your README</h3>
+			<span class="inline whitespace-normal">
+				Navigate to your Neovim config on Dotfyle at <code class="bg-black px-1 py-0.5 rounded">profile > config </code>. If you're logged in you will have an option to navigate to your readme.
+        If not, you can add <code class="bg-black px-1 py-0.5 rounded">/readme</code> to the end of the url. Copy
+				the markdown from this page and use it on your GitHub readme.
+			</span>
+			<span class="inline whitespace-normal">
+				A good way to find your config page is to login.
+			</span>
+
+			<GithubLoginButton />
+		</section>
+	</GlossyCard>
 	<section class="flex flex-col gap-4">
 		<GlossyCard>
 			<div class="flex flex-col gap-2 p-4">
 				<h3>Configs with Dotfyle generated README</h3>
 
 				<span class="whitespace-normal">
-          These neovim configs added the Dotfyle generated README to their GitHub. The list updates once a day.
-        </span>
+					These neovim configs added the Dotfyle generated README to their GitHub. The list updates
+					once a day.
+				</span>
 			</div>
 		</GlossyCard>
 		{#await data.loading.configs then configs}
 			<BigGridContainer>
 				{#each configs as config, _}
-				<NeovimConfigCard
-					slug={config.slug}
-					repo={config.repo}
-					owner={config.owner}
-					avatar={config.ownerAvatar}
-					initFile={config.initFile}
-					root={config.root}
-					stars={config.stars.toString()}
-					pluginManager={config.pluginManager ?? 'unknown'}
-					pluginCount={config.pluginCount.toString()}
-					showGithubLink={false}
-				/>
+					<NeovimConfigCard
+						slug={config.slug}
+						repo={config.repo}
+						owner={config.owner}
+						avatar={config.ownerAvatar}
+						initFile={config.initFile}
+						root={config.root}
+						stars={config.stars.toString()}
+						pluginManager={config.pluginManager ?? 'unknown'}
+						pluginCount={config.pluginCount.toString()}
+						showGithubLink={false}
+					/>
 				{/each}
 			</BigGridContainer>
 		{/await}
