a1bae0899b9aa09a7e6b566b69c360240a205a12
codico <codicocodes@gmail.com>
2023-07-29 16:58:47 +0200

    Reduce sampling rate

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,7 +13,7 @@ import { prismaClient } from '$lib/server/prisma/client';
 if (NODE_ENV === 'production') {
 	Sentry.init({
 		dsn: env.SENTRY_DSN,
-		tracesSampleRate: 1,
+		tracesSampleRate: 0.1,
 		environment: NODE_ENV,
 		integrations: [new Sentry.Integrations.Prisma({ client: prismaClient })]
 	});
