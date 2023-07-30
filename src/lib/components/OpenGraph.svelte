6a137e3c320b2c270bc992f4dd5054d163e2b86f
codico <codicocodes@gmail.com>
2023-07-30 21:12:32 +0200

    Add OpenGraph component

---
+++ src/lib/components/OpenGraph.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let url: string;
+</script>
+
+<meta property="og:type" content="website" />
+<meta property="og:title" content={title} />
+<meta name="description" content={description} />
+<meta property="og:url" content={url} />
+<meta property="og:image" content="/dotfyle.png" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@codicocodes" />
+<meta name="twitter:creator" content="@codicocodes" />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+<meta name="twitter:image" content="/dotfyle.png" />
