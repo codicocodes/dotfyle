bddcef1bc6654252bdd592d455fa675b4774802b
codico <codicocodes@gmail.com>
2023-07-30 21:57:57 +0200

    Improve OpenGraph

--- src/lib/components/OpenGraph.svelte
+++ src/lib/components/OpenGraph.svelte
@@ -7,12 +7,12 @@
 <meta name="description" content={description} />
 <meta property="og:type" content="website" />
 <meta property="og:title" content={title} />
-<meta property="og:description" content={description} />
+<meta property="og:description" content={description.replaceAll("#", "")} />
 <meta property="og:url" content={url} />
 <meta property="og:image" content="/dotfyle.png" />
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@codicocodes" />
 <meta name="twitter:creator" content="@codicocodes" />
 <meta name="twitter:title" content={title} />
-<meta name="twitter:description" content={description} />
+<meta name="twitter:description" content={description.replaceAll("#", "")} />
 <meta name="twitter:image" content="/dotfyle.png" />
