8af5cfad5f9ec2e781a41108d4af83ae8a15c885
codico <codicocodes@gmail.com>
2023-08-22 22:43:01 +0200

    Improve sharing on TWiN issue

---
+++ src/lib/components/ShareContainer.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { copyToClipboard } from '$lib/utils';
+	import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+	import {
+		faCopy,
+		faEnvelope
+	} from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+  export let url: string
+  export let tweetText: string
+  export let emailSubject: string
+  export let emailBody: string
+	$: tweetUrl = `https://twitter.com/intent/tweet?url=${url}&text=${tweetText}&via=codicocodes`;
+</script>
+<div class="flex my-4 items-center justify-center w-full gap-2">
+	<button
+		title="Copy url"
+		class="block p-4 bg-gray-800 rounded-full"
+		on:click={() => copyToClipboard(url)}
+	>
+		<Fa icon={faCopy} />
+	</button>
+	<a
+		title="Share on Twitter"
+		href={tweetUrl}
+		target="blank"
+		class="block p-4 bg-gray-800 rounded-full"
+	>
+		<Fa icon={faTwitter} />
+	</a>
+	<a
+		class="block p-4 bg-gray-800 rounded-full"
+		href="mailto:?subject={emailSubject}&amp;body={emailBody}: {url}"
+		title="Share by Email"
+	>
+		<Fa icon={faEnvelope} />
+	</a>
+</div>
--- src/routes/this-week-in-neovim/[issue]/+page.svelte
+++ src/routes/this-week-in-neovim/[issue]/+page.svelte
@@ -3,33 +3,29 @@
 	import Button from '$lib/components/Button.svelte';
 	import HtmlContent from '$lib/components/HtmlContent.svelte';
 	import OpenGraph from '$lib/components/OpenGraph.svelte';
+	import ShareContainer from '$lib/components/ShareContainer.svelte';
 	import SmallTitle from '$lib/components/SmallTitle.svelte';
 	import { updateLatestReadTwinPost } from '$lib/services/twin';
-	import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-	import {
-		faChevronLeft,
-		faChevronRight,
-		faEnvelope,
-	} from '@fortawesome/free-solid-svg-icons';
-	import Fa from 'svelte-fa';
+	import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	$: url = `https://dotfyle.com/this-week-in-neovim/${data.post.issue}`;
 	$: tweetText = `This Week in Neovim ${data.post.title.replace('#', '')}`;
 	$: tweetUrl = `https://twitter.com/intent/tweet?url=${url}&text=${tweetText}&via=codicocodes`;
-  $: {
-    if (browser) {
-      updateLatestReadTwinPost(data.post.issue)
-    }
-  }
+	$: {
+		if (browser) {
+			updateLatestReadTwinPost(data.post.issue);
+		}
+	}
 </script>
 
 <svelte:head>
-  <title>{data.post.title}</title>
+	<title>{data.post.title}</title>
 	<OpenGraph
 		title={data.post.title}
 		url="https://dotfyle.com/this-week-in-neovim/{data.post.issue}"
-		description="This Week in Neovim {data.post.issue} with news and updates from the Neovim plugin ecosystem and Neovim core."
+		description="This Week in Neovim {data.post
+			.issue} with news and updates from the Neovim plugin ecosystem and Neovim core."
 	/>
 </svelte:head>
 
@@ -44,6 +40,13 @@
 		</time>
 	</h5>
 {/if}
+
+<ShareContainer
+	{url}
+	{tweetText}
+	emailSubject="Checkout This Week in Neovim {data.post.issue}"
+	emailBody="Check out {data.post.title.replaceAll('#', '')}"
+/>
 
 <div class="flex justify-between p-2">
 	{#if data.post.issue > 1}
@@ -75,30 +78,3 @@
 		/>
 	</a>
 {/if}
-
-<div class="flex flex-col mx-2 my-4 items-center justify-center w-full gap-2">
-	<div class="space-y-2">
-		<h4 class="text-2xl font-medium tracking-wide">Share This Week in Neovim</h4>
-		<div class="flex gap-2">
-			<a
-				title="Share on Twitter"
-				href={tweetUrl}
-				target="blank"
-				class="block p-4 bg-blue-400 rounded-full"
-			>
-				<Fa icon={faTwitter} />
-			</a>
-			<a
-				class="block p-4 bg-orange-600 rounded-full"
-				href="mailto:?subject=Checkout This Week in Neovim {data.post
-					.issue}&amp;body=Check out {data.post.title.replaceAll(
-					'#',
-					''
-				)}: https://www.dotfyle.com/this-week-in-neovim/{data.post.issue}"
-				title="Share by Email"
-			>
-				<Fa icon={faEnvelope} />
-			</a>
-		</div>
-	</div>
-</div>
