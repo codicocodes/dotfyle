52e16ba661bf30ea31460a24dccd8a230df8d2ba
codico <codicocodes@gmail.com>
2023-08-26 22:34:17 +0200

    Format

--- src/lib/components/PluginSearchNavigation.svelte
+++ src/lib/components/PluginSearchNavigation.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	$page.url;
 	const links = [
 		{ label: 'Discover', url: '/neovim/plugins/trending' },
 		{ label: 'Colorschemes', url: '/neovim/colorscheme/trending' },
-		{ label: 'Configurations', url: '/neovim/configurations/top' },
+		{ label: 'Configurations', url: '/neovim/configurations/top' }
 	];
 </script>
 
 <div class="w-full flex gap-2 mt-2 text-sm">
 	{#each links as link}
-		<a href={link.url} class="{$page.url.pathname.includes(link.url.split("/")[2]) ? "bg-white text-black" : "bg-white/10 hover:bg-white text-white hover:text-black"} p-2 rounded-full font-semibold"> {link.label} </a>
+		<a
+			href={link.url}
+			class="{$page.url.pathname.includes(link.url.split('/')[2])
+				? 'bg-white text-black'
+				: 'bg-white/10 hover:bg-white text-white hover:text-black'} p-2 rounded-full font-semibold"
+		>
+			{link.label}
+		</a>
 	{/each}
 </div>
