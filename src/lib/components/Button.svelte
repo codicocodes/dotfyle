567b4f292d7314549e33b8313850546c7ff6c130
codico <codicocodes@gmail.com>
2023-08-26 18:06:20 +0200

    Format

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,29 +4,35 @@
 	import { DoubleBounce } from 'svelte-loading-spinners';
 	export let text: string;
 	export let icon: IconDefinition;
-  export let loading = false
-  export let iconPosition: 'right' | 'left' = 'right'
-  export let disabled = false
-  export let type: "submit" | "button" | "reset" | null | undefined = "submit";
-  export let formaction: string | undefined = undefined
+	export let loading = false;
+	export let iconPosition: 'right' | 'left' = 'right';
+	export let disabled = false;
+	export let type: 'submit' | 'button' | 'reset' | null | undefined = 'submit';
+	export let formaction: string | undefined = undefined;
 </script>
 
 <button
-  formaction={formaction}
-  on:click
-	class={`${disabled || loading ? "cursor-not-allowed" : ""} bg-white/30  p-2 py-1 xl:px-2 xl:py-1 rounded flex gap-1 items-center ${loading ? "hover:cursor-not-allowed hover:bg-white/10" : "shadow-lg hover:shadow-green-300/25 hover:bg-gradient-to-br hover:from-cyan-500 hover:to-green-500 "}`}
-	type={type}
-  disabled={disabled || loading}
+	{formaction}
+	on:click
+	class={`${
+		disabled || loading ? 'cursor-not-allowed' : ''
+	} bg-white/30  p-2 py-1 xl:px-2 xl:py-1 rounded flex gap-1 items-center ${
+		loading
+			? 'hover:cursor-not-allowed hover:bg-white/10'
+			: 'shadow-lg hover:shadow-green-300/25 hover:bg-gradient-to-br hover:from-cyan-500 hover:to-green-500 '
+	}`}
+	{type}
+	disabled={disabled || loading}
 >
-  {#if iconPosition === 'right'}
-    {text}
-  {/if}
-  {#if loading}
-	  <DoubleBounce color="#15be97" size="8" />
-  {:else}
-    <Fa class="ml-1" size="xs" {icon} />
-  {/if}
-  {#if iconPosition === 'left'}
-    {text}
-  {/if}
+	{#if iconPosition === 'right'}
+		{text}
+	{/if}
+	{#if loading}
+		<DoubleBounce color="#15be97" size="8" />
+	{:else}
+		<Fa class="ml-1" size="xs" {icon} />
+	{/if}
+	{#if iconPosition === 'left'}
+		{text}
+	{/if}
 </button>
