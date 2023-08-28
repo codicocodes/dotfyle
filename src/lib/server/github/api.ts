a80626a1d00caf1b4dfc5e5969d251daa1c9c1f7
codico <codicocodes@gmail.com>
2023-08-28 22:46:30 +0200

    Format

--- src/lib/server/github/api.ts
+++ src/lib/server/github/api.ts
@@ -2,17 +2,17 @@ import { Octokit } from '@octokit/rest';
 import { z } from 'zod';
 import { GithubBlob, GithubRepository, GithubTree } from './schema';
 
-export const fetchGitCommits = async(token: string, since: Date, owner: string, repo: string) => {
+export const fetchGitCommits = async (token: string, since: Date, owner: string, repo: string) => {
 	const gh = new Octokit({
 		auth: `token ${token}`
 	});
-  const commitResponse = await gh.repos.listCommits({
-    since: since.toString(),
-    owner,
-    repo,
-  })
-  return commitResponse.data
-}
+	const commitResponse = await gh.repos.listCommits({
+		since: since.toString(),
+		owner,
+		repo
+	});
+	return commitResponse.data;
+};
 
 export const fetchGithubRepositories = async (
 	username: string,
@@ -21,10 +21,7 @@ export const fetchGithubRepositories = async (
 	const gh = new Octokit({
 		auth: `token ${token}`
 	});
-  const reposData = await gh.paginate(
-    gh.repos.listForUser,
-    { username, per_page: 100 }
-  )
+	const reposData = await gh.paginate(gh.repos.listForUser, { username, per_page: 100 });
 	const repos = z.array(GithubRepository).parse(reposData);
 	return repos;
 };
@@ -47,31 +44,42 @@ export const fetchRepoFileTree = async (
 	token: string,
 	owner: string,
 	repo: string,
-  branch: string,
+	branch: string
 ): Promise<GithubTree> => {
 	const gh = new Octokit({
 		auth: `token ${token}`
 	});
-	const pagesResponse = await gh.git.getTree({ owner, repo, tree_sha: branch, recursive: 'true' });
+	const pagesResponse = await gh.git.getTree({
+		owner,
+		repo,
+		tree_sha: branch,
+		recursive: 'true'
+	});
 	const data = pagesResponse.data;
 	const tree = GithubTree.parse(data);
 	return tree;
 };
 
-export const fetchFile = async (token: string, owner: string, repo: string, file_sha: string): Promise<GithubBlob> => {
+export const fetchFile = async (
+	token: string,
+	owner: string,
+	repo: string,
+	file_sha: string
+): Promise<GithubBlob> => {
 	const gh = new Octokit({
 		auth: `token ${token}`
 	});
-  const blobResponse = await gh.git.getBlob({ owner, repo, file_sha})
-  return GithubBlob.parse(blobResponse.data)
-}
+	const blobResponse = await gh.git.getBlob({ owner, repo, file_sha });
+	return GithubBlob.parse(blobResponse.data);
+};
 
 export const fetchReadme = async (token: string, owner: string, repo: string): Promise<string> => {
 	const gh = new Octokit({
 		auth: `token ${token}`
 	});
-  const readmeResponse = await gh.repos.getReadme({ owner, repo })
-  const readme = GithubBlob.parse(readmeResponse.data)
-  const content = Buffer.from(readme.content, 'base64').toString();
-  return content
-}
+	const readmeResponse = await gh.repos.getReadme({ owner, repo });
+	const readme = GithubBlob.parse(readmeResponse.data);
+	const content = Buffer.from(readme.content, 'base64').toString();
+	return content;
+};
+
