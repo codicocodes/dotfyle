d6d5d4aaf13b2292a608d8d594deb93b76870cf4
codico <codicocodes@gmail.com>
2023-09-08 22:03:02 +0200

    Add canonical link to OG

--- src/lib/components/OpenGraph.svelte
+++ src/lib/components/OpenGraph.svelte
@@ -2,18 +2,20 @@
 	export let title: string;
 	export let description: string;
 	export let url: string;
-  export let image = "/dotfyle.png"
+	export let image = '/dotfyle.png';
 </script>
 
 <meta name="description" content={description} />
 <meta property="og:type" content="website" />
 <meta property="og:title" content={title} />
-<meta property="og:description" content={description.replaceAll("#", "")} />
+<meta property="og:description" content={description.replaceAll('#', '')} />
 <meta property="og:url" content={url} />
-<meta property="og:image" content="{image}" />
+<meta property="og:image" content={image} />
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@codicocodes" />
 <meta name="twitter:creator" content="@codicocodes" />
 <meta name="twitter:title" content={title} />
-<meta name="twitter:description" content={description.replaceAll("#", "")} />
-<meta name="twitter:image" content="{image}" />
+<meta name="twitter:description" content={description.replaceAll('#', '')} />
+<meta name="twitter:image" content={image} />
+<link rel="canonical" href="{url}" />
+
