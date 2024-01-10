6ce0831bbe138ffe089a71bdd9d65275d91b4aad
codico <codicocodes@gmail.com>
2024-01-10 23:27:25 +0100

    Refactor plugin content fetching

--- src/lib/server/twin/builder.ts
+++ src/lib/server/twin/builder.ts
@@ -1,7 +1,6 @@
 import { prismaClient } from '../prisma/client';
 import { readFileSync } from 'fs';
 import { daysAgo, getMediaType } from '$lib/utils';
-import type { Media, NeovimPlugin } from '@prisma/client';
 import { getRecentCoreCommits } from './getRecentCoreCommits';
 
 export class IssueAlreadyPublished extends Error {
@@ -23,60 +22,42 @@ class CoreCommitContentFetcher implements ContentFetcher {
 		const newCoreCommitTemplate = readFileSync(this.template, 'utf8');
 		const commits = await getRecentCoreCommits();
 
-		const content = []
+		const content = [];
 		for (const commit of commits) {
 			if (daysAgo(days) < commit.date) {
-				let commitContent = newCoreCommitTemplate
+				let commitContent = newCoreCommitTemplate;
 				commitContent = commitContent.replaceAll('{title}', commit.title);
 				commitContent = commitContent.replaceAll('{body}', commit.body);
 				commitContent = commitContent.replaceAll('{url}', commit.url);
-				content.push(commitContent)
+				content.push(commitContent);
 			}
 		}
 		return content.join('');
 	}
 }
 
-export class TwinPostBuilder {
-	newPluginTemplate = './twin/new-plugin-template.md';
-	template = './twin/template.md';
-	contentFetchers: ContentFetcher[] = [new CoreCommitContentFetcher()];
+class PluginContentFetcher implements ContentFetcher {
+	section = '{new-plugins-section}';
+	template = './twin/new-plugin-template.md';
 
-	async validate(issue: number) {
-		const twinIssue = await prismaClient.twinPost.findUnique({
-			where: {
-				issue
-			}
-		});
-		if (!twinIssue) return;
-		if (!twinIssue.publishedAt) return;
-		throw new IssueAlreadyPublished();
-	}
+	async getContent(days: number): Promise<string> {
+		const newPluginTemplate = readFileSync(this.template, 'utf8');
+		const plugins = await this.getNewPlugins(days);
+		const content = [];
 
-	async run(issue: number, days: number) {
-		const newPlugins = await this.getNewPlugins(days);
-		let content = await this.appendNewPlugins(newPlugins);
-
-		for (const contentFetcher of this.contentFetchers) {
-			content = content.replace(contentFetcher.section, await contentFetcher.getContent(days));
+		for (const plugin of plugins) {
+			let post = newPluginTemplate.replaceAll('{fullname}', `${plugin.owner}/${plugin.name}`);
+			post = post.replaceAll(
+				'{image}',
+				plugin.media.filter((m) => getMediaType(m) === 'image')[0]?.url ?? ''
+			);
+			post = post.replaceAll('{category}', plugin.category);
+			post = post.replaceAll('{description}', plugin.shortDescription);
+			post = post.replaceAll('{githubUrl}', `https://github.com/${plugin.owner}/${plugin.name}`);
+			post = post.replaceAll('{dotfyleUrl}', `/plugins/${plugin.owner}/${plugin.name}`);
+			content.push(post);
 		}
-		const title = `Issue #${issue}: ${newPlugins
-			.map((p) => p.name)
-			.slice(0, 3)
-			.join(', ')}`;
-		const post = {
-			title,
-			content,
-			issue,
-			publishedAt: null
-		};
-		return await prismaClient.twinPost.upsert({
-			where: {
-				issue
-			},
-			create: post,
-			update: post
-		});
+		return content.join('\n\n');
 	}
 
 	async getNewPlugins(days: number) {
@@ -100,29 +81,43 @@ export class TwinPostBuilder {
 		});
 		return plugins;
 	}
+}
 
-	async getCoreCommitsContent() { }
+export class TwinPostBuilder {
+	template = './twin/template.md';
+	contentFetchers: ContentFetcher[] = [new PluginContentFetcher(), new CoreCommitContentFetcher()];
 
-	async appendNewPlugins(plugins: (NeovimPlugin & { media: Media[] })[]) {
-		const template = readFileSync(this.template, 'utf8');
+	async validate(issue: number) {
+		const twinIssue = await prismaClient.twinPost.findUnique({
+			where: {
+				issue
+			}
+		});
+		if (!twinIssue) return;
+		if (!twinIssue.publishedAt) return;
+		throw new IssueAlreadyPublished();
+	}
 
-		const newPluginTemplate = readFileSync(this.newPluginTemplate, 'utf8');
+	async run(issue: number, days: number) {
+		let content = readFileSync(this.template, 'utf8');
 
-		const newPlugins = [];
-
-		for (const plugin of plugins) {
-			let post = newPluginTemplate.replaceAll('{fullname}', `${plugin.owner}/${plugin.name}`);
-			post = post.replaceAll(
-				'{image}',
-				plugin.media.filter((m) => getMediaType(m) === 'image')[0]?.url ?? ''
-			);
-			post = post.replaceAll('{category}', plugin.category);
-			post = post.replaceAll('{description}', plugin.shortDescription);
-			post = post.replaceAll('{githubUrl}', `https://github.com/${plugin.owner}/${plugin.name}`);
-			post = post.replaceAll('{dotfyleUrl}', `/plugins/${plugin.owner}/${plugin.name}`);
-			newPlugins.push(post);
+		for (const contentFetcher of this.contentFetchers) {
+			content = content.replace(contentFetcher.section, await contentFetcher.getContent(days));
 		}
+		const title = `Issue #${issue}`;
 
-		return template.replaceAll('{new-plugins-section}', newPlugins.join('\n\n'));
+		const post = {
+			title,
+			content,
+			issue,
+			publishedAt: null
+		};
+		return await prismaClient.twinPost.upsert({
+			where: {
+				issue
+			},
+			create: post,
+			update: post
+		});
 	}
 }
