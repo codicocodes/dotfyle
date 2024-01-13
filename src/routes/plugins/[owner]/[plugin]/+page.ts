057148e6f8fafa5a09c91b26ccff168fb28f606f
codico <codicocodes@gmail.com>
2024-01-13 18:56:01 +0100

    Sort media as expected

--- src/routes/plugins/[owner]/[plugin]/+page.ts
+++ src/routes/plugins/[owner]/[plugin]/+page.ts
@@ -4,22 +4,21 @@ import type { PageLoad, PageLoadEvent } from './$types';
 
 export const load: PageLoad = async function load(event: PageLoadEvent) {
 	const { owner, plugin: name } = event.params;
-	const [[plugin, categoryPlugins], configs, breaking, media] = await Promise.all([
+	const [[plugin, categoryPlugins], configs, breaking] = await Promise.all([
 		trpc(event)
 			.getPlugin.query({ owner, name })
 			.then(async (plugin) => {
 				const relatedPlugins = await trpc(event).getPluginsByCategory.query(plugin.category);
-        return [plugin, relatedPlugins] as const
+				return [plugin, relatedPlugins] as const;
 			}),
 		trpc(event).getConfigsForPlugin.query({ owner, name }),
 		trpc(event).getBreakingCommits.query({ owner, name }),
-		trpc(event).getMediaForPlugin.query({ owner, name })
 	]);
 	return {
 		plugin,
 		configs: configs as unknown as NeovimConfigWithMetaData[],
 		categoryPlugins,
 		breaking,
-		media
+		media: plugin.media
 	};
 };
