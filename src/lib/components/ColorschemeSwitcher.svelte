ab022525eb0a124e773ea4055ca40a08596499a2
codico <codicocodes@gmail.com>
2024-01-14 14:51:03 +0100

    Update ColorschemeSwitcher

--- src/lib/components/ColorschemeSwitcher.svelte
+++ src/lib/components/ColorschemeSwitcher.svelte
@@ -3,7 +3,6 @@
 	import { faPalette } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 	import Modal from './Modal.svelte';
-	import MultiSelectFilter from './MultiSelectFilter.svelte';
 	import ThemeSwitcher from './ThemeSwitcher.svelte';
 
 	let isOpen = false;
@@ -23,6 +22,7 @@
 
 <button
 	aria-label="Change colorscheme"
+	data-umami-event="Change colorscheme - Started"
 	type="button"
 	on:click={() => {
 		isOpen = true;
@@ -37,13 +37,15 @@
 		isOpen = false;
 	}}
 >
-	<div class="flex justify-center items-center mb-8 mt-4">
+	<div class="flex justify-center items-center mb-4 mt-4">
 		<ThemeSwitcher />
 	</div>
 	<div class="my-4 w-full mt-2 z-50 text-lg font-semibold flex flex-col gap-2" id="user-menu">
 		{#each colorschemes as theme}
 			<button
-				class="flex w-full sm:min-w-80 py-2 px-4 items-center justify-center bg-black/30 rounded-lg text-black {theme.bg}"
+				class="flex w-full sm:min-w-80 py-2 px-4 items-center justify-center bg-black/30 rounded-lg text-black {theme.bg} border-[2px] border-accent-muted hover:border-accent-bright"
+				data-umami-event="Change colorscheme - Done"
+				data-umami-event-theme={theme.name}
 				on:click={() => {
 					setColorscheme(theme.name);
 					isOpen = false;
--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -9,11 +9,14 @@
 
 <button
 	aria-label={isLightMode ? 'Switch to dark mode' : 'Switch to light mode'}
+	title={isLightMode ? 'Switch to dark mode' : 'Switch to light mode'}
 	type="button"
+	class="flex gap-4 w-full bg-black/30 p-4 rounded-xl justify-center items-center border-[1px] border-accent-muted hover:border-accent-bright"
 	on:click={() => {
 		isLightMode = !isLightMode;
 		setTheme(isLightMode ? 'light' : 'dark');
 	}}
 >
 	<Fa icon={isLightMode ? faSun : faMoon} size="1.5x" />
+	<span>{isLightMode ? 'light mode' : 'dark mode'}</span>
 </button>
