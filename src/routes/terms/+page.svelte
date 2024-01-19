facc64ee7a1556275320c38f6b1ce439b3fa35b7
codico <codicocodes@gmail.com>
2024-01-19 22:19:29 +0100

    Add terms

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,96 @@
+<h1>Terms and conditions</h1>
+
+<h2>1. Terms</h2>
+
+<p>
+	By accessing this Website, accessible from https://dotfyle.com, you are agreeing to be bound by
+	these Website Terms and Conditions of Use and agree that you are responsible for the agreement
+	with any applicable local laws. If you disagree with any of these terms, you are prohibited from
+	accessing this site. The materials contained in this Website are protected by copyright and trade
+	mark law.
+</p>
+
+<h2>2. Use License</h2>
+
+<p>
+	Permission is granted to temporarily download one copy of the materials on Dotfyle's Website for
+	personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer
+	of title, and under this license you may not: modify or copy the materials; use the materials for
+	any commercial purpose or for any public display; attempt to reverse engineer any software
+	contained on Dotfyle's Website; remove any copyright or other proprietary notations from the
+	materials; or transferring the materials to another person or "mirror" the materials on any other
+	server. This will let Dotfyle to terminate upon violations of any of these restrictions. Upon
+	termination, your viewing right will also be terminated and you should destroy any downloaded
+	materials in your possession whether it is printed or electronic format. These Terms of Service
+	has been created with the help of the Terms Of Service Generator.
+</p>
+<h2>3. Disclaimer</h2>
+
+<p>
+	All the materials on Dotfyle's Website are provided "as is". Dotfyle makes no warranties, may it
+	be expressed or implied, therefore negates all other warranties. Furthermore, Dotfyle does not
+	make any representations concerning the accuracy or reliability of the use of the materials on its
+	Website or otherwise relating to such materials or any sites linked to this Website.
+</p>
+
+<h2>4. Limitations</h2>
+
+<p>
+	Dotfyle or its suppliers will not be hold accountable for any damages that will arise with the use
+	or inability to use the materials on Dotfyle's Website, even if Dotfyle or an authorize
+	representative of this Website has been notified, orally or written, of the possibility of such
+	damage. Some jurisdiction does not allow limitations on implied warranties or limitations of
+	liability for incidental damages, these limitations may not apply to you.
+</p>
+
+<h2>5. Revisions and Errata</h2>
+
+<p>
+	The materials appearing on Dotfyle's Website may include technical, typographical, or photographic
+	errors. Dotfyle will not promise that any of the materials in this Website are accurate, complete,
+	or current. Dotfyle may change the materials contained on its Website at any time without notice.
+	Dotfyle does not make any commitment to update the materials.
+</p>
+
+<h2>6. Links</h2>
+
+<p>
+	Dotfyle has not reviewed all of the sites linked to its Website and is not responsible for the
+	contents of any such linked site. The presence of any link does not imply endorsement by Dotfyle
+	of the site. The use of any linked website is at the user's own risk.
+</p>
+
+<h2>7. Site Terms of Use Modifications</h2>
+
+<p>
+	Dotfyle may revise these Terms of Use for its Website at any time without prior notice. By using
+	this Website, you are agreeing to be bound by the current version of these Terms and Conditions of
+	Use.
+</p>
+
+<h2>8. Your Privacy</h2>
+
+<p>Please read the <a href="/privacy">Privacy Policy</a>.</p>
+
+<h2>9. Governing Law</h2>
+
+<p>
+	Any claim related to Dotfyle's Website shall be governed by the laws of Sweden without regards to
+	its conflict of law provisions.
+</p>
+
+<style lang="postcss">
+	h1 {
+		@apply text-3xl sm:text-5xl font-extrabold tracking-wider my-8;
+	}
+	h2 {
+		@apply text-3xl sm:text-4xl font-bold tracking-wider my-2;
+	}
+	p,
+	ul {
+		@apply text-xl tracking-wide my-8 leading-8;
+	}
+	a {
+		@apply text-transparent bg-clip-text bg-gradient-primary;
+	}
+</style>
