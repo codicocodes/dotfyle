94824e48aca3c283e9c94a463e4906b119c8e89d
codico <codicocodes@gmail.com>
2024-01-21 22:18:50 +0100

    Fix add config

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import AddNewConfig from '$lib/components/add/AddNewConfig.svelte';
 	import { fade } from 'svelte/transition';
-	import type { PageData } from './$types';
 	import { page } from '$app/stores';
 	import type { GithubRepository } from '$lib/server/github/schema';
 	import { trpc } from '$lib/trpc/client';
 	import { onMount } from 'svelte';
+	import session from '$lib/stores/session';
 
-	export let data: PageData;
-	const { user } = data;
 
 	let repositories: GithubRepository[] = [];
 	let loading = true;
@@ -35,11 +33,11 @@
 	<h1 out:fade class="my-12 md:my-20 text-2xl font-semibold tracking-tight">
 		Add a new Neovim config
 	</h1>
-	{#if loading}
+	{#if $session.loading || loading}
 		<div class="w-2 h-2 rounded-full bg-main animate-pulse"/>
-	{:else if error}
+	{:else if error || !$session.user}
 		{error}
 	{:else}
-		<AddNewConfig {user} {repositories} />
+		<AddNewConfig user={$session.user} {repositories} />
 	{/if}
 </div>
