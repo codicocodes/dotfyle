37b9f0899f6efb18137d82ffad5189b0fd80c0cd
codico <codicocodes@gmail.com>
2024-01-21 22:20:11 +0100

    Fix welcome page

---
+++ src/routes/welcome/+page.server.ts
@@ -0,0 +1,10 @@
+import { verifyToken } from '$lib/server/auth/services';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad, PageServerLoadEvent } from './$types';
+
+export const load: PageServerLoad = async (event: PageServerLoadEvent) => {
+  const user = verifyToken(event.cookies)
+  if (!user) {
+    throw redirect(302, "/api/auth/github?next=welcome")
+  }
+};
--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import AddNewConfig from '$lib/components/add/AddNewConfig.svelte';
-	import type { PageData } from './$types';
 	import { page } from '$app/stores';
 	import type { GithubRepository } from '$lib/server/github/schema';
 	import { trpc } from '$lib/trpc/client';
 	import { onMount } from 'svelte';
 	import HeroTitle from '$lib/components/HeroTitle.svelte';
 	import CoolText from '$lib/components/CoolText.svelte';
-
-	export let data: PageData;
-	const { user } = data;
+	import session from '$lib/stores/session';
 
 	let repositories: GithubRepository[] = [];
 	let loading = true;
@@ -34,14 +31,14 @@
 	<HeroTitle>
 		Welcome to <CoolText text="Dotfyle" />
 	</HeroTitle>
-	{#if loading}
+	{#if loading || $session.loading}
 		<div class="flex flex-col gap-2 items-center">
 			<div class="w-2 h-2 rounded-full bg-main animate-pulse"/>
 			<h2 class="text-xl font-light tracking-wide">loading github repositories</h2>
 		</div>
-	{:else if error}
+	{:else if error || !$session.user}
 		{error}
 	{:else}
-		<AddNewConfig {user} {repositories} />
+		<AddNewConfig user={$session.user} {repositories} />
 	{/if}
 </div>
