a4dfb055f9f238677de76b50b8a10d4f72297880
codico <codicocodes@gmail.com>
2024-01-21 20:54:44 +0100

    Cleanup

--- src/routes/neovim/plugins/rss.xml/+server.ts
+++ src/routes/neovim/plugins/rss.xml/+server.ts
@@ -1,9 +1,8 @@
-import { getCachedPluginFeed, rebuildCachedPluginFeed } from "$lib/server/rss/plugins";
+import { getCachedPluginFeed } from "$lib/server/rss/plugins";
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
 	const cachedFeed = await getCachedPluginFeed()
-	console.log(cachedFeed)
 	return new Response(cachedFeed, {
 		headers: {
 			'Cache-Control': `max-age=0, s-maxage=${60 * 60 * 24}`, // seconds
