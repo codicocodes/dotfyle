aa52037f8bff8f1b8d858c0c852908f8bddaa251
codico <codicocodes@gmail.com>
2024-01-21 21:27:27 +0100

    Remove me

--- src/routes/[username]/+page.server.ts
+++ src/routes/[username]/+page.server.ts
@@ -3,10 +3,9 @@ import type { PageServerLoad, PageServerLoadEvent } from './$types';
 
 export const load: PageServerLoad = async function load(event: PageServerLoadEvent) {
 	const username = event.params.username;
-  return {
-    me: trpc(event).getUser.query(),
-    profile: trpc(event).getUserByUsername.query(username),
-    configs: trpc(event).getConfigsByUsername.query(username),
-    plugins: trpc(event).getAuthoredPluginsByUsername.query(username)
-  };
+	return {
+		profile: trpc(event).getUserByUsername.query(username),
+		configs: trpc(event).getConfigsByUsername.query(username),
+		plugins: trpc(event).getAuthoredPluginsByUsername.query(username)
+	};
 };
