c9292e2924ff7c757e797900aab9a9ef504c4f7a
codico <codicocodes@gmail.com>
2024-01-21 15:56:59 +0100

    Add plugins RSS

--- src/routes/this-week-in-neovim/rss.xml/+server.ts
+++ src/routes/this-week-in-neovim/rss.xml/+server.ts
@@ -5,17 +5,17 @@ import { marked } from 'marked';
 import { sanitizeHtml } from '$lib/utils';
 
 // @TODO: Move caching to redis
-
-let cachedFeed: string | undefined
+let cachedFeed: string | undefined;
 
 export async function rebuildCachedTwinFeed() {
-  cachedFeed = await createTwinRssFeed()
+	cachedFeed = await createTwinRssFeed();
 }
 
 async function createTwinRssFeed() {
 	const feed = new RSS({
-		title: 'This Week in Neovim RSS Feed',
-    description: 'This Week In Neovim is a weekly newsletter with updates from the Neovim ecosystem, including new plugins and breaking changes.',
+		title: 'This Week in Neovim',
+		description:
+			'This Week In Neovim is a weekly newsletter with updates from the Neovim ecosystem, including new plugins and breaking changes.',
 		site_url: 'https://dotfyle.com/this-week-in-neovim',
 		feed_url: 'https://dotfyle.com/this-week-in-neovim/rss'
 	});
@@ -28,30 +28,30 @@ async function createTwinRssFeed() {
 		orderBy: {
 			issue: 'desc'
 		},
-    take: 10,
+		take: 10
 	});
 
 	for (const post of posts) {
 		if (!post.publishedAt) continue;
-    const html = marked(post.content)
-    const description = await sanitizeHtml(html)
+		const html = marked(post.content);
+		const description = await sanitizeHtml(html);
 		feed.item({
 			title: post.title,
 			url: `https://dotfyle.com/this-week-in-neovim/${post.issue}?utm_source=twin-rss`,
 			date: post.publishedAt,
-			description,
+			description
 		});
 	}
 
-  const xml = feed.xml({ indent: true })
+	const xml = feed.xml({ indent: true });
 
-  return xml
+	return xml;
 }
 
 export const GET: RequestHandler = async () => {
-  if (!cachedFeed) {
-    cachedFeed = await createTwinRssFeed()
-  }
+	if (!cachedFeed) {
+		cachedFeed = await createTwinRssFeed();
+	}
 	return new Response(cachedFeed, {
 		headers: {
 			'Cache-Control': `max-age=0, s-maxage=${60 * 60 * 24}`, // seconds
