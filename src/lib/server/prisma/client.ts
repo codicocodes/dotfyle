ae9adb0745429a627340582ecbe95bce9581c9f5
codico <codicocodes@gmail.com>
2024-02-10 19:05:55 +0100

    Use public url during build time and private during runtime

--- src/lib/server/prisma/client.ts
+++ src/lib/server/prisma/client.ts
@@ -1,8 +1,15 @@
+import { DATABASE_PRIVATE_URL } from '$env/static/private';
 import Prisma from '@prisma/client';
 
 const { PrismaClient } = Prisma;
 
-export const prismaClient = new PrismaClient();
+export const prismaClient = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_PRIVATE_URL,
+    }
+  }
+});
 
 prismaClient.$on('beforeExit', async () => {
   console.log('beforeExit hook')
