228a35962b2577f6a8b37283ea0b9fbb26251eed
codico <codicocodes@gmail.com>
2024-02-11 23:16:36 +0100

    Format

--- src/lib/components/NeovimPluginMetaData.svelte
+++ src/lib/components/NeovimPluginMetaData.svelte
@@ -54,61 +54,62 @@
 		</span>
 	</div>
 	<div class="flex flex-grow justify-end gap-2">
-	{#if configCount > 0}
-		<a
-			class="relative px-2 rounded-full text-xs flex gap-1 items-center font-medium bg-white text-black hover:bg-accent-muted"
-			href="/neovim/configurations/top?plugins={name}">
-			<Fa icon={faUsers} />
-			{configCount} <span class="hidden sm:inline">configs </span>
-		</a>
-	{/if}
-	{#if links.length > 0}
-		<button
-			class="relative px-2 rounded-full text-xs flex gap-1 items-center font-medium bg-white text-black hover:bg-accent-muted"
-			on:click|stopPropagation={handleSeeLinks}
-			on:click|stopPropagation
-			data-umami-event="Plugin Mention - Open"
-			title="Mentions of the plugin in this config"
-		>
-			{#if isOpen}
-				<div
-					class="absolute right-0 top-10 z-50 bg-accent-muted text-white rounded"
-					id="user-menu"
-					on:blur={() => (isOpen = false)}
-				>
-					<div class="flex flex-col w-full h-full bg-base-900 sm:bg-transparent">
-						{#each links as link}
-							<CoolTextOnHover>
-								<a
-									data-umami-event="Plugin Mention - Navigate"
-									class="px-4 py-2 flex gap-2 items-center"
-									target="_blank"
-									href={link}
-								>
-									<div class="force-white-text">
-										<Fa icon={faExternalLink} class="text-base-100" />
-									</div>
-									<span class="whitespace-nowrap">
-										{link.replace(
-											/(https:\/\/github.com\/)([a-zA-Z0-9]+)\/[a-zA-Z0-9]+\/blob\/([a-zA-Z0-9-]+)\//,
-											''
-										)}
-									</span>
-								</a>
-							</CoolTextOnHover>
-						{/each}
+		{#if configCount > 0}
+			<a
+				class="relative px-2 rounded-full text-xs flex gap-1 items-center font-medium bg-white text-black hover:bg-accent-muted"
+				href="/neovim/configurations/top?plugins={name}"
+			>
+				<Fa icon={faUsers} />
+				{configCount} <span class="hidden sm:inline">configs </span>
+			</a>
+		{/if}
+		{#if links.length > 0}
+			<button
+				class="relative px-2 rounded-full text-xs flex gap-1 items-center font-medium bg-white text-black hover:bg-accent-muted"
+				on:click|stopPropagation={handleSeeLinks}
+				on:click|stopPropagation
+				data-umami-event="Plugin Mention - Open"
+				title="Mentions of the plugin in this config"
+			>
+				{#if isOpen}
+					<div
+						class="absolute right-0 top-10 z-50 bg-accent-muted text-white rounded"
+						id="user-menu"
+						on:blur={() => (isOpen = false)}
+					>
+						<div class="flex flex-col w-full h-full bg-base-900 sm:bg-transparent">
+							{#each links as link}
+								<CoolTextOnHover>
+									<a
+										data-umami-event="Plugin Mention - Navigate"
+										class="px-4 py-2 flex gap-2 items-center"
+										target="_blank"
+										href={link}
+									>
+										<div class="force-white-text">
+											<Fa icon={faExternalLink} class="text-base-100" />
+										</div>
+										<span class="whitespace-nowrap">
+											{link.replace(
+												/(https:\/\/github.com\/)([a-zA-Z0-9]+)\/[a-zA-Z0-9]+\/blob\/([a-zA-Z0-9-]+)\//,
+												''
+											)}
+										</span>
+									</a>
+								</CoolTextOnHover>
+							{/each}
+						</div>
 					</div>
-				</div>
-			{/if}
-			<Fa icon={faAt} />
-			<span class="flex gap-1">
-				{links.length}
-				<span class="hidden sm:flex items-center gap-1">
-					{links.length === 1 ? 'mention' : 'mentions'}
-					<Fa icon={isOpen ? faChevronUp : faChevronDown} />
+				{/if}
+				<Fa icon={faAt} />
+				<span class="flex gap-1">
+					{links.length}
+					<span class="hidden sm:flex items-center gap-1">
+						{links.length === 1 ? 'mention' : 'mentions'}
+						<Fa icon={isOpen ? faChevronUp : faChevronDown} />
+					</span>
 				</span>
-			</span>
-		</button>
-	{/if}
-</div>
+			</button>
+		{/if}
+	</div>
 </div>
