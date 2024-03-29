0ea4f61970e2543356b63c5f566e688dd5d846fa
codico <codicocodes@gmail.com>
2024-03-29 16:54:17 +0100

    Add email subscription form

---
+++ src/routes/this-week-in-neovim/subscribe/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { browser } from '$app/environment';
+	import { getTheme } from '$lib/theme';
+	if (browser) {
+		const theme = getTheme();
+		if (theme === 'dark')
+			window.fd('form', {
+				formId: '6606dd05f4a205deae5f1915',
+				containerEl: '#fd-form-6606dd05f4a205deae5f1915'
+			});
+
+		if (theme === 'light')
+			window.fd('form', {
+				formId: '6606e0680835cf37369dbe00',
+				containerEl: '#fd-form-6606e0680835cf37369dbe00'
+			});
+	}
+</script>
+
+<head>
+	<script>
+		(function (w, d, t, h, s, n) {
+			w.FlodeskObject = n;
+			var fn = function () {
+				(w[n].q = w[n].q || []).push(arguments);
+			};
+			w[n] = w[n] || fn;
+			var f = d.getElementsByTagName(t)[0];
+			var v = '?v=' + Math.floor(new Date().getTime() / (120 * 1000)) * 60;
+			var sm = d.createElement(t);
+			sm.async = true;
+			sm.type = 'module';
+			sm.src = h + s + '.mjs' + v;
+			f.parentNode.insertBefore(sm, f);
+			var sn = d.createElement(t);
+			sn.async = true;
+			sn.noModule = true;
+			sn.src = h + s + '.js' + v;
+			f.parentNode.insertBefore(sn, f);
+		})(window, document, 'script', 'https://assets.flodesk.com', '/universal', 'fd');
+	</script>
+</head>
+
+<div id="fd-form-6606dd05f4a205deae5f1915" />
+<div id="fd-form-6606e0680835cf37369dbe00" />
