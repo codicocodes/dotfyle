731502f0807d0ea6c2f1432a4a4588d8529d4796
codico <codicocodes@gmail.com>
2024-03-30 02:00:15 +0100

    Remove share buttons from SearchHeader

--- src/lib/components/SearchHeader.svelte
+++ src/lib/components/SearchHeader.svelte
@@ -5,14 +5,17 @@
 	import CoolTextWithChildren from './CoolTextWithChildren.svelte';
 	import MediumHeroTitle from './MediumHeroTitle.svelte';
 	import Search from './Search.svelte';
-	import ShareContainer from './ShareContainer.svelte';
+	interface Post {
+		title: string
+		description: string
+	}
 
-	export let content: any;
+	export let content: Post;
 	export let placeholder: string;
 	export let navigation: any[];
 </script>
 
-<div class="flex flex-col w-full items-center my-8 gap-4">
+<div class="flex flex-col w-full items-center gap-4">
 	<meta class="" itemprop="dateUpdated" content={new Date().toLocaleDateString()} />
 	<MediumHeroTitle>
 		<CoolText text={content.title} />
@@ -21,16 +24,6 @@
 		{content.description}
 	</p>
 </div>
-
-<div class="flex w-full justify-center">
-	<ShareContainer
-		url={$page.url.origin + $page.url.pathname}
-		tweetText={content.title}
-		emailSubject={content.title}
-		emailBody={content.description}
-	/>
-</div>
-
 <Search {placeholder} />
 <div class="flex flex-col w-full items-center mt-4 gap-4">
 	<div class="flex gap-6 font-medium text-lg">
