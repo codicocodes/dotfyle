555b40c67fcbc7c99f825ea20e80361790907a23
codicocodes <codicocodes@gmail.com>
2024-11-17 15:34:43 +0100

    Fix incorrect migration file

--- prisma/migrations/20240922092246_neovim_config_unique_for_id_user_id/migration.sql
+++ prisma/migrations/20240922092246_neovim_config_unique_for_id_user_id/migration.sql
@@ -4,5 +4,5 @@
   - A unique constraint covering the columns `[id,userId]` on the table `NeovimConfig` will be added. If there are existing duplicate values, this will fail.
 
 */-- CreateIndex
-CREATEUNIQUEINDEX "NeovimConfig_id_userId_key"
+CREATE UNIQUE INDEX "NeovimConfig_id_userId_key"
   ON "NeovimConfig"("id", "userId");
