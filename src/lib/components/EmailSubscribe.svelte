89c3a0f1fbba74b752a004d01cdacc783f003f25
codicocodes <codicocodes@gmail.com>
2026-03-28 14:17:31 +0100

    Disable newsletter signups properly

--- src/lib/components/EmailSubscribe.svelte
+++ src/lib/components/EmailSubscribe.svelte
@@ -25,23 +25,22 @@
 		target="_blank"
 	>
 		<div
-			class="flex gap-2 w-full sm:w-3/4 md:w-2/3 xl:w-1/2 items-center p-2 sm:p-3 rounded-lg text-black text-sm font-medium focus:outline-none focus:border-main shadow-xl focus:shadow-main/25 focus:ring-1 focus:ring-main bg-white {isfocused
-				? 'shadow-xl shadow-main/25'
-				: ''}"
+			class="flex gap-2 w-full sm:w-3/4 md:w-2/3 xl:w-1/2 items-center p-2 sm:p-3 rounded-lg text-sm font-medium shadow-xl bg-white/40 opacity-75 cursor-not-allowed"
+			title="New subscriptions are currently paused"
 		>
 			<input
 				name="email_address"
 				type="email"
 				required
 				placeholder="example@email.com"
-				class="bg-transparent focus:outline-none w-full"
-				on:focus={() => (isfocused = true)}
-				on:blur={() => (isfocused = false)}
+				class="bg-transparent focus:outline-none w-full cursor-not-allowed text-gray-400"
+				disabled
 			/>
 		</div>
-		<div class="mx-2">
+		<div class="mx-2" title="New subscriptions are currently paused">
 			<button
-				class="flex justify-between transition-all border-accent-muted bg-black/30 py-2 px-4 rounded-lg border-[1px] flex items-center hover:border-secondary font-semibold text-lg gap-2"
+				class="flex justify-between transition-all border-accent-muted bg-black/30 py-2 px-4 rounded-lg border-[1px] flex items-center font-semibold text-lg gap-2 opacity-75 cursor-not-allowed"
+				disabled
 			>
 				Subscribe
 			</button>
